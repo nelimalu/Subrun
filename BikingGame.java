0b1ee1bd0335a9843b313fbde9a6077dd185d282
Luka Jovanovic <lukaj2501@gmail.com>
2023-06-09 01:33:52 -0400

    more commenting

--- BikingGame.java
+++ BikingGame.java
@@ -225,19 +225,40 @@ public class BikingGame {
      * Can be moved around like an obstacle
      */
     public static class SampleGame {
-        /**  */
+        /** Lanes used in the game */
         private Lane[] lanes;
+
+        /** Arraylist to store each vehicle: Arraylist because they are easily added to and removed from */
         private ArrayList<Vehicle> vehicles;
+
+        /** Teacher npc that explains the game and quizzes the player */
         private Teacher teacher;
+
+        /** Second teacher used because distance detection is easy, and detects if the player entered the biking area to begin the game */
         private Teacher enterRadius;
+
+        /** Y position of the game */
         private int y;
+
+        /** number that is incremented once every frame */
         private long frame;
+
+        /** Has the dialogue been read yet? */
         private boolean readDialogue;
+
+        /** Is the user currently in dialogue with the NPC? */
         private boolean inDialogue;
+
+        /** Is the user currently playing the game? */
         private boolean playing;
+
+        /** the current lane that the player is in: Right most is 0, middle is 1, left is 2 */
         private int currentLane;
+
+        /** prompt used to give the player info after the game is completed */
         private MessageBox prompt;
 
+        /** List of messages to display in the dialogue, as well as the question with answers */
         public static final MessageBox[] dialogue = new MessageBox[] {
                 new MessageBox("Howdy! Welcome to the testing level for biking in the suburbs: aka surviving as a cyclist."),
                 new MessageBox("In this game mode, you must use left and right arrow keys to bike on a car filled road and make it to your destination."),
@@ -252,7 +273,18 @@ public class BikingGame {
                 new MessageBox("Correct! In Canada, it is illegal for a cyclist to bike on a sidewalk.", 2),
                 new MessageBox("Wrong! Feel free to try again!"),
         };
+
+        /**
+         * The constructor for the sample game
+         * Creates lanes with set spawn rates, positions, and colours
+         * Creates teacher and sets position
+         * @param x X position
+         * @param y Y position
+         * @param width Width of the game
+         * @param height Height of the game
+         */
         public SampleGame(int x, int y, int width, int height) {
+            // create lanes with set spawn rates, positions, and colours
             lanes = new Lane[] {
                     new Lane(x, y, 100, 2000, new Color(70, 70, 70), 0.005),
                     new Lane(x + 100, y, 100, 2000, new Color(100, 100, 100), 0.015),
@@ -262,39 +294,75 @@ public class BikingGame {
             readDialogue = false;
             inDialogue = false;
             playing = false;
-            currentLane = 1;
+            currentLane = 1; // start in middle lane: Lanes are in format of 0, 1, 2
             frame = 0;
             vehicles = new ArrayList<Vehicle>();
             teacher = new Teacher(655, -80, new Sprite("assets/bikingMan.png", 6));
             enterRadius = new Teacher(655, -150);
         }
 
+        /**
+         * @return Is the player currently in dialogue with the npc
+         */
         public boolean isInDialogue() {
             return inDialogue;
         }
 
+        /**
+         * Changes whether the player is currently in dialogue or not
+         * @param set If the player is in dialogue or not
+         */
         public void setDialogue(boolean set) {
             inDialogue = set;
         }
+
+        /**
+         * Changes whether the player as read the dialogue or not
+         * @param set If the player has read the dialogue or not
+         */
         public void setReadDialogue(boolean set) {
             readDialogue = set;
         }
 
+        /**
+         * @return The lane that the player is currently in
+         */
         public int getCurrentLane() {
             return currentLane;
         }
 
+        /**
+         * Changes which lane the player is in
+         * @param amount The lane to move to
+         */
         public void setCurrentLane(int amount) {
             currentLane = amount;
         }
+
+        /**
+         * @return The prompt message to be displayed after finishing the sample game
+         */
         public MessageBox getPrompt() {
             return prompt;
         }
+
+        /**
+         * Resets the prompt
+         */
         public void clearPrompt() {
             prompt = null;
         }
+
+        /**
+         * @return Whether the player is playing the biking minigame
+         */
         public boolean isPlaying() { return playing; }
 
+        /**
+         * Move the game and everything inside it in relation to the player's movement
+         * @param xDistance X distance travelled
+         * @param yDistance Y distance travelled
+         */
         public void move(int xDistance, int yDistance) {
 
             for (BikingGame.Vehicle vehicle : vehicles) {
@@ -310,66 +378,76 @@ public class BikingGame {
             enterRadius.moveX(xDistance);
             enterRadius.moveY(yDistance);
 
-            y += yDistance;
+            y += yDistance;  // keep track of the Y distance moved
         }
 
+        /**
+         * Main loop for the sample biking game
+         * @param g Graphics object for drawing
+         * @param player Player
+         * @param xOffset X distance travelled from the origin of the maze level
+         * @param yOffset Y distance travelled from the origin of the maze level
+         * @param maze The maze level
+         * @return Whether the player should be sent to the origin of the maze level
+         */
         public boolean draw(Graphics g, Player player, int xOffset, int yOffset, Maze maze) {
-
-
-            if (frame > 750) {
+            if (frame > 750) {  // if the player survived for long enough
                 playing = false;
                 frame = 0;
                 player.resetPosition();
-                Maze.finishedLevels[1] = true;
-                prompt = new MessageBox("Congratulations! You made it to your destination!");
-                return true;
+                Maze.finishedLevels[1] = true;  // fill the star corresponding to the bike game
+                prompt = new MessageBox("Congratulations! You made it to your destination!");  // set prompt
+                return true;  // go back to start
             }
 
-            if (teacher.inRadius(player)) {
-                if (!readDialogue)
+            if (teacher.inRadius(player)) {  // if player is near the teacher
+                if (!readDialogue)  // if player has not read the dialogue
                     inDialogue = true;
             }
-            if (!playing && enterRadius.inRadius(player)) {
+            if (!playing && enterRadius.inRadius(player)) {  // if player enters area and is not currently playing
                 clearPrompt();
                 frame = 0;
-                playing = true;
+                playing = true;  // player is now playing
                 player.resetAnimationCount();
-                maze.move(-295 - xOffset, 0);
+                maze.move(-295 - xOffset, 0);  // line up player with lanes
             }
 
             if (playing) {
-                frame++;
+                frame++;  // increase frame
+                // move player up slowly until the bottom of the maze level cannot be seen
                 if (yOffset < 600)
                     maze.move(0, 5);
             }
 
-            for (Lane lane : lanes) {
+            for (Lane lane : lanes) {  // draw and update each frame
                 lane.draw(g);
                 lane.update(vehicles, playing);
             }
 
             ArrayList<Integer> toRemove = new ArrayList<Integer>();
-            for (int i = 0; i < vehicles.size(); i++) {
+            for (int i = 0; i < vehicles.size(); i++) {  // loop through each vehicle
                 vehicles.get(i).draw(g);
-                if (player.collideSmall(vehicles.get(i))) {
+                if (player.collideSmall(vehicles.get(i))) {  // if player collides with vehicle
+                    // end game
                     playing = false;
                     player.resetPosition();
                     vehicles.clear();
                     currentLane = 1;
-                    prompt = new MessageBox("You got hit by a car! I guess the streets aren't that safe after all...");
+                    prompt = new MessageBox("You got hit by a car! I guess the streets aren't that safe after all...");  // set prompt
                     return true;
                 }
-                if (vehicles.get(i).move(y - 30)) // out of bounds
-                    toRemove.add(i);
+                if (vehicles.get(i).move(y - 30)) // if the vehicle moves out of the bounds of the game
+                    toRemove.add(i);  // delete it to same memory
             }
 
+            // weird array copying to delete the useless cars
             for (int i = 0; i < toRemove.size(); i++)
                 toRemove.set(i, toRemove.get(i) - i);
 
             for (int index : toRemove) vehicles.remove(index);
 
-            teacher.draw(g);
-            return false;
+            teacher.draw(g);  // draw the teacher
+            return false;  // do not send the player back to the start
         }
     }
 
--- WalkingGame.java
+++ WalkingGame.java
@@ -4,21 +4,47 @@ import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.awt.*;
 
+/**
+ * WalkingGame class handles and draws the walking game, both in the escape room and maze room
+ * Inner classes are used for handling repetitiveness within the class
+ *
+ * <strong>Course info:</strong>
+ * ICS4U0 with V. Krasteva
+ *
+ * @version 1.0
+ * @author Luka Jovanovic & Brian Song
+ * Created on 2023/06/04
+ */
 public class WalkingGame {
 
+    /** arraylist to store each lane in the game */
     private ArrayList<Lane> lanes;
+
+    /** arraylist to store each car in the game */
     private ArrayList<Car> cars;
+
+    /** width of the game */
     private static int WIDTH = 2000;
+
+    /** height of the game */
     private static int HEIGHT = 150;
-    private long frame;
+
+    /** obstacles to keep the player within the bounds of the game */
     private Obstacle[] obstacles;
+
+    /** current score of the player, measured by distance travelled up the screen */
     private int score;
+
+    /** font used for drawing in this class */
     private static Font scoreFont = new Font("Monospaced", Font.BOLD, 20);
 
+    /**
+     * Constructor for the walking game in the escape room
+     * creates the lanes, with three grass and one road guaranteed
+     */
     public WalkingGame() {
         this.lanes = new ArrayList<Lane>();
         this.cars = new ArrayList<Car>();
-        this.frame = 0;
         this.obstacles = new Obstacle[] {};
 
         for (int i = 0; i < 2; i++) {
@@ -34,51 +60,66 @@ public class WalkingGame {
         }
     }
 
+    /**
+     * Main game loop for the biking game in the escape room
+     * handles updates and graphics
+     * @param g Graphics object for drawing
+     * @param yOffset Y distance travelled by the player while in the game
+     * @param player the player
+     * @param room the escape room object
+     */
     public void paint(Graphics g, int yOffset, Player player, EscapeRoom room) {
-        frame++;
-
         ArrayList<Lane> copyLanes = new ArrayList<>(lanes);
         for (Lane lane : lanes) {
             lane.draw(g);
-            lane.updateReal(cars);
+            lane.updateReal(cars);  // create new cars based on spawn rates
 
-            if (lane.getY() > 1000) {
+            if (lane.getY() > 1000) {  // check if the lane is off the screen
+                // move the lane to the top: creates a cycle of reusing lanes to save memory
                 lane.setY(lanes.get(lanes.size() - 1).getY() - HEIGHT);
                 lane.setFrequency(lane.getFrequency() + 0.01);
                 copyLanes.remove(0);
                 copyLanes.add(lane);
             }
         }
+        // copy so we can send lanes to the front without disturbing the for loop
         lanes = copyLanes;
 
         ArrayList<Integer> toRemove = new ArrayList<Integer>();
         for (int i = 0; i < cars.size(); i++) {
             cars.get(i).draw(g);
-            if (player.collide(cars.get(i))) {
+            if (player.collide(cars.get(i))) {  // check if the player collided with a car
                 room.setGame("DEAD");
-                if (score >= 5000) {
+                if (score >= 5000) {  // if the player completed the task
                     room.setGame("ALIVE");
-                    room.setWalkHighScore(score);
-                    room.walkPerson.getMessage().setTextArray("You have successfully completed this task! (score > 5000)", 1);
+                    room.setWalkHighScore(score);  // set new high score
+                    room.walkPerson.getMessage().setTextArray("You have successfully completed this task! (score > 5000)", 1);  // change message in the npc
                 }
             }
-            if (cars.get(i).move(-700, WIDTH - 100))
-                toRemove.add(i);
+            if (cars.get(i).move(-700, WIDTH - 100))  // if the car is off the screen
+                toRemove.add(i);  // remove the car
         }
 
+        // funky copying to delete useless cars non-destructively
         for (int i = 0; i < toRemove.size(); i++)
             toRemove.set(i, toRemove.get(i) - i);
 
         for (int index : toRemove) cars.remove(index);
 
-        if (yOffset > score)
+        if (yOffset > score)  // increase score when the player moves up the screen (does not count moving down and back up)
             score = yOffset;
 
+        // draw current score on top left of screen
         g.setColor(Color.BLACK);
         g.setFont(scoreFont);
         g.drawString("Score: " + score, 10, 30);
     }
 
+    /**
+     * Moves the lanes and cars when the player moves
+     * @param xDistance X distance travelled
+     * @param yDistance Y distance travelled
+     */
     public void move(int xDistance, int yDistance) {
         for (Car car : cars) {
             car.moveX(xDistance);
@@ -90,24 +131,54 @@ public class WalkingGame {
         }
     }
 
+    /**
+     * @return obstacles in the game used as boundaries
+     */
     public Obstacle[] getObstacles() {
         return obstacles;
     }
 
+    /**
+     * @return Player's current score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Generates a random integer between a minumum and maximum value
+     * @param min Minimum value
+     * @param max Maximum value
+     * @return random integer between the minimum and maximum
+     */
     public static int randint(int min, int max) {
         return (int) (Math.random() * (max - min)) + min;
     }
 
+    /**
+     * Inner lane class to generate cars
+     */
     private static class Lane extends Obstacle {
+        /** car flow direction of the lane (1 is right, -1 is left) */
         private int direction;
+
+        /** speed at which cars travel in the lane */
         private int speed;
+
+        /** is the lane a road or grass */
         private boolean isRoad;
+
+        /** frequency at which cars spawn in the lane */
         private double frequency;
 
+        /**
+         * Constructor for either grass or road lanes
+         * randomly picks direction and speed of the lane
+         * @param x
+         * @param y
+         * @param width
+         * @param height
+         */
         public Lane(int x, int y, int width, int height) {
             super(x, y, width, height);
             this.direction = Math.random() >= 0.5 ? -1 : 1;
@@ -116,27 +187,52 @@ public class WalkingGame {
             this.frequency = 0;
         }
 
+        /**
+         * Constructor for road lanes
+         * sets the spawn frequency of the road
+         * @param x
+         * @param y
+         * @param width
+         * @param height
+         * @param frequency % chance a car will spawn in a frame
+         */
         public Lane(int x, int y, int width, int height, double frequency) {
             this(x, y, width, height);
             this.isRoad = true;
             this.frequency = frequency;
         }
 
+        /**
+         * Move the lane based on player movement
+         * @param xDistance X distance travelled
+         * @param yDistance Y distance travelled
+         */
         public void move(int xDistance, int yDistance) {
             moveX(xDistance);
             moveY(yDistance);
         }
 
+        /**
+         * @return The frequency at which cars spawn in this lane
+         */
         public double getFrequency() {
             return frequency;
         }
 
+        /**
+         * Change the frequency at which cars spawn in this lane
+         * @param amount The new frequency
+         */
         public void setFrequency(double amount) {
             frequency = amount;
         }
 
+        /**
+         * Draws the lane
+         * @param g Graphics object for drawing
+         */
         public void draw(Graphics g) {
-
+            // change colour depending on if its a road or grass
             if (isRoad)
                 g.setColor(new Color(168, 168, 168));
             else
@@ -144,16 +240,27 @@ public class WalkingGame {
             g.fillRect(getX(), getY(), getWidth(), getHeight());
         }
 
+        /**
+         * Generate new cars in the sample game based on the spawn rates
+         * @param frame the current frame of the game
+         * @param cars the car arraylist
+         */
         public void update(long frame, ArrayList<Car> cars) {
-            if (isRoad && frame % (frequency * 25L) == 0) {
+            if (isRoad && frame % (frequency * 25L) == 0) {  // if the lane is a road, and the frequency is reached
+                // create and add a car
                 Sprite sprite = new Sprite("assets/car.png", 5);
                 int x = direction > 0 ? getX() : getX() + getWidth() - sprite.getImage().getWidth();
                 cars.add(new Car(x, getY() + getHeight() / 3, direction, speed, sprite));
             }
         }
 
+        /**
+         * Generate new cars in the real escape room game based on random number generation
+         * @param cars the car arraylist
+         */
         public void updateReal(ArrayList<Car> cars) {
-            if (isRoad && Math.random() < frequency) {
+            if (isRoad && Math.random() < frequency) {  // if the car is a road and it should spawn
+                // create and add a dcar
                 Sprite sprite = new Sprite("assets/car.png", 5);
                 int x = direction > 0 ? getX() : getX() + getWidth() - sprite.getImage().getWidth();
                 cars.add(new Car(x, getY() + getHeight() / 3, direction, speed, sprite));
@@ -161,30 +268,52 @@ public class WalkingGame {
         }
     }
 
+    /**
+     * Inner car class
+     */
     private static class Car extends Obstacle {
+        /** direction that the car travels in (1 is right, -1 is left */
         private int direction;
+
+        /** speed at which the car travels */
         private int speed;
 
+        /**
+         * Constructor for the car, similar as obstacle
+         * @param x
+         * @param y
+         * @param direction Direction which the car travels in
+         * @param speed Speed of the car
+         * @param sprite Car sprite
+         */
         public Car(int x, int y, int direction, int speed, Sprite sprite) {
             super(x, y, sprite);
             this.direction = direction;
             this.speed = speed;
         }
 
+        /**
+         * Draw the car moving in the right direction
+         * @param g Graphics object for drawing
+         */
         public void draw(Graphics g) {
-            if (Controller.SHOW_HITBOXES) {
+            if (Controller.SHOW_HITBOXES) {  // draw hitboxes of cars if they should be drawn
                 g.setColor(Color.WHITE);
                 g.drawRect(getX(), getY(), getWidth(), getHeight());
             }
 
-            if (direction == 1)
+            if (direction == 1)  // draw car normally if it's going right
                 g.drawImage(getSprite().getImage(), getX(), getY(), null);
-            else
+            else  // otherwise flip the sprite and draw it
                 g.drawImage(getSprite().getImage(), getX() + getWidth(), getY(), -getWidth(), getHeight(), null);
-
-
         }
 
+        /**
+         * move the car, and decide if it's out of bounds and should be deleted
+         * @param x X position at which it should be deleted
+         * @param width Width of the lane
+         * @return if the car should be deleted or not
+         */
         public boolean move(int x, int width) {
             moveX(speed * direction);
 
@@ -193,17 +322,38 @@ public class WalkingGame {
 
     }
 
+    /**
+     * Inner sample game class of the walking game that can be implemented as an object within the maze game
+     */
     public static class SampleGame {
-
+        /** arraylist to store each lane in the game */
         private ArrayList<Car> cars;
+
+        /** arraylist to store each car in the game */
         private ArrayList<Lane> lanes;
+
+        /** X position of the game */
         private int x;
+
+        /** width of the sample game, how long the lanes are */
         private int width;
+
+        /** frame integer that increments once each frame */
         private long frame;
+
+        /** teacher npc that explains the game and quizzes the player on the topic */
         private Teacher teacher;
+
+        /** if the player is currently in dialogue with the npc */
         private boolean inDialogue;
+
+        /** if the player already went through the dialogue with the npc */
         private boolean readDialogue;
+
+        /** prompt to give the player a message after finishing the game */
         private MessageBox prompt;
+
+        /** List of messages to display in the dialogue, as well as the question with answers */
         public static final MessageBox[] dialogue = new MessageBox[] {
                 new MessageBox("Hey there! Welcome to the testing level for walking in the suburbs: aka surviving as a pedestrian."),
                 new MessageBox("In this game mode, you must cross the streets without getting hit by a car."),
@@ -219,6 +369,15 @@ public class WalkingGame {
                 new MessageBox("Wrong! Feel free to try again!"),
         };
 
+        /**
+         * Constructor for the walking sample game
+         * creates a set of 15 lanes with predetermined grass and concrete lanes
+         * spawn rates are also pre determined
+         * @param x
+         * @param y
+         * @param width
+         * @param height
+         */
         public SampleGame(int x, int y, int width, int height) {
             this.lanes = new ArrayList<Lane>();
             this.cars = new ArrayList<Car>();
@@ -239,26 +398,48 @@ public class WalkingGame {
             }
         }
 
+        /**
+         * @return is the user currently in dialogue with the npc
+         */
         public boolean isInDialogue() {
             return inDialogue;
         }
 
+        /**
+         * Changes whether the player is in dialogue
+         * @param inDialogue new boolean representing the player in dialogue
+         */
         public void setDialogue(boolean inDialogue) {
             this.inDialogue = inDialogue;
         }
 
+        /**
+         * Changes whether the player has gone through the dialogue
+         * @param set new boolean representing if the player has read the dialogue
+         */
         public void setReadDialogue(boolean set) {
             readDialogue = set;
         }
 
+        /**
+         * @return The prompt that should be displayed after the game is played
+         */
         public MessageBox getPrompt() {
             return prompt;
         }
 
+        /**
+         * clear the prompt to free up screen space
+         */
         public void clearPrompt() {
             prompt = null;
         }
 
+        /**
+         * move the walking game in relation to player movement
+         * @param xDistance X distance travelled
+         * @param yDistance Y distance travelled
+         */
         public void move(int xDistance, int yDistance) {
 
             for (Car car : cars) {
@@ -272,11 +453,12 @@ public class WalkingGame {
             teacher.moveX(xDistance);
             teacher.moveY(yDistance);
 
-            this.x = lanes.get(0).getX();
+            this.x = lanes.get(0).getX();  // keep track of the X location of the game
         }
 
         /**
-         * @param g Graphics
+         * Main game loop for the walking sample game
+         * @param g Graphics object for drawing
          * @param player Player
          * @return If the map should be reset or not
          */
@@ -298,14 +480,15 @@ public class WalkingGame {
             ArrayList<Integer> toRemove = new ArrayList<Integer>();
             for (int i = 0; i < cars.size(); i++) {
                 cars.get(i).draw(g);
-                if (player.collide(cars.get(i))) {
+                if (player.collide(cars.get(i))) {  // if the player hits a car
                     prompt = new MessageBox("You got hit by a car! I guess the streets aren't that safe after all...");
                     return true;
                 }
-                if (cars.get(i).move(x, width))
+                if (cars.get(i).move(x, width))  // move the car, check if it should be deleted
                     toRemove.add(i);
             }
 
+            // funky way to delete cars non-destructively
             for (int i = 0; i < toRemove.size(); i++)
                 toRemove.set(i, toRemove.get(i) - i);
 
